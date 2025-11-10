abcae36156b1e4c4d7f71db23c0917b9b1721844
Raheem <akapsyjr@gmail.com>
2025-11-10 23:07:13 +0100

    omo

--- Dashboard.js
+++ Dashboard.js
@@ -1,16 +1,58 @@
-const token = localStorage.getItem('accessToken');
-const logoutBtn = document.getElementById('logoutBtn');
-logoutBtn.addEventListener('click', () => {
-  localStorage.removeItem('accessToken');
-  window.location.href = 'login.html';
+const token = localStorage.getItem("accessToken");
+const logoutBtn = document.getElementById("logoutBtn");
+
+const totalDebtsOwedElem = document.getElementById("totalDebtsOwed");
+const creditSalesElem = document.getElementById("creditSales");
+const paymentsReceivedElem = document.getElementById("paymentsReceived");
+const activeDebtorsElem = document.getElementById("activeDebtors");
+const overduePaymentsElem = document.getElementById("overduePayments");
+
+currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+const day = String(currentDate.getDate()).padStart(2, "0");
+const formattedDate = `${year}-${month}-${day}`;
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("accessToken");
+  window.location.href = "login.html";
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (!token) {
-        window.location.href = 'login.html';
-        return;
+getStats = async () => {
+  try {
+    const response = await fetch(
+      `https://gbese-6f0j.onrender.com/api/admin/stats?start=2025-11-01&end=${formattedDate}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.ok) {
+      totalDebtsOwedElem.textContent = `₦${data.statistics.totalDebtOwed}`;
+      creditSalesElem.textContent = `₦${data.statistics.creditSales}`;
+      paymentsReceivedElem.textContent = `₦${data.statistics.paymentReceived}`;
+      activeDebtorsElem.textContent = data.statistics.activeDebtors;
+      overduePaymentsElem.textContent = `₦${data.statistics.overduePayments}`;
+    } else {
+      console.error("Failed to fetch stats:", data.message);
     }
-    else {
-        document.querySelector('.dashboard').style.display = 'block';
-    }
+  } catch (error) {
+    console.error("Error fetching stats:", error);
+  } finally {
+    console.log("Fetch stats attempt finished.");
+    document.querySelector(".dashboard").style.display = "block";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (!token) {
+    window.location.href = "login.html";
+    return;
+  } else {
+    getStats();
+  }
 });
